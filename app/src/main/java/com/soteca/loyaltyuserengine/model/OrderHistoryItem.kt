4a21b811a415f8bacd77b5826bdb74a58f394eb4
Measna <measna@haricrm.com>
2018-07-02 14:26:59 +0700

    Model order Item

---
+++ app/src/main/java/com/soteca/loyaltyuserengine/model/OrderHistoryItem.kt
@@ -0,0 +1,9 @@
+package com.soteca.loyaltyuserengine.model
+
+class OrderHistoryItem(quantity: Int) : OrderItem(quantity) {
+    // TODO: field Product
+    // TODO: field Package
+    // TODO: constructor (product, quantity)
+    // TODO: constructor (package, quantity)
+
+}
