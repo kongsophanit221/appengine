9cb2e1b68148426ce25e93897117b0dcd689a206
Nit <sophanith@haricrm.com>
2018-07-16 16:02:42 +0700

    - Clean some code on DefaultEntity Enum Class

--- app/src/main/java/com/soteca/loyaltyuserengine/model/BaseItem.kt
+++ app/src/main/java/com/soteca/loyaltyuserengine/model/BaseItem.kt
@@ -190,25 +190,4 @@ class Datasource(val context: Context) : AppDatasource {
 
     }
 
-    enum class DefaultEntity(val value: String) {
-        ANNOTATION("annotation"),
-        PRODUCT("idcrm_posproduct"),
-        CATEGORY("idcrm_poscategory"),
-        COMPONENT("idcrm_poscomponent");
-
-        companion object {
-            fun from(findValue: String): DefaultEntity = DefaultEntity.values().first { it.value == findValue }
-        }
-
-        val primaryIdAttribute: String
-            get() {
-                return when (this) {
-                    DefaultEntity.ANNOTATION -> "annotationid"
-                    DefaultEntity.PRODUCT -> "idcrm_posproductid"
-                    DefaultEntity.CATEGORY -> "idcrm_poscategoryid"
-                    DefaultEntity.COMPONENT -> "idcrm_poscomponentid"
-                }
-            }
-    }
-
 }
